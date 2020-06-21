666550182d09b834893f9eda1eaae73442f8f481
Raden Ramadista <raden.ramadista@gmail.com>
2020-06-21 11:39:21 +0700

    First commit

---
+++ controllers/pos_title.js
@@ -0,0 +1,27 @@
+const handlePosTitle = (req, res, db) => {
+	const page = parseInt(req.query.page) || 1;
+	const per_page = parseInt(req.query.per_page) || 10;
+	const offset = (page - 1) * per_page;
+	const results = {};
+
+	db.select('*')
+		.from('pa_rating')
+		.offset(offset)
+		.limit(per_page)
+		.then((data) => {
+			if (data.length) {
+				// results.total = total.count;
+				results.per_page = per_page;
+				results.page = page;
+				results.results = data;
+				res.json(results);
+			} else {
+				res.status(400).json('Data not found');
+			}
+		})
+		.catch((err) => res.status(400).json('Error getting data'));
+};
+
+module.exports = {
+	handlePosTitle: handlePosTitle,
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+
+const pa_rating = require('./controllers/pa_rating');
+const office = require('./controllers/office');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'Kimora1205',
+		database: 'mega',
+	},
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('It is working!');
+});
+
+app.get('/pa_rating', (req, res) => {
+	pa_rating.handlePARating(req, res, db);
+});
+
+app.get('/office', (req, res) => {
+	office.handleOffice(req, res, db);
+});
+
+// app.get('/pa_rating', (req, res) => {
+// 	pa_rating.handlePARating(req, res, db);
+// });
+
+app.listen(5000, () => {
+	console.log('App is running on port 5000');
+});
